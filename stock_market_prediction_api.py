6a95f6e8c186161541ac61578e05b7c5a5c11467
PramodIngale <85428570+PramodIngale@users.noreply.github.com>
2021-11-08 20:17:04 +0530

    Update stock_market_prediction_api.py

--- stock_market_prediction_api.py
+++ stock_market_prediction_api.py
@@ -315,9 +315,9 @@ else:
     x_train, y_train = datapreparation(data_scaled)
     
     if selected_stock not in ['SBIN', 'INFY', 'DMART']:
-        selected_stock = "Others"
+        selected_stock_rename = "Others"
 
-    neuron1, dropout_rate,epochs, batch_size = best_parameters[selected_stock].values()
+    neuron1, dropout_rate,epochs, batch_size = best_parameters[selected_stock_rename].values()
     
     predicted_value = model_building_prediction(neuron1, dropout_rate, x_train, y_train, epochs, batch_size, data_scaled, scaler, 10)
     st.write("___________________________________________________________")
