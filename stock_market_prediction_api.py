2831d09db442ed25784dcf6ed7fefe09974578e1
PramodIngale <85428570+PramodIngale@users.noreply.github.com>
2021-11-08 17:15:03 +0530

    Update stock_market_prediction_api.py

--- stock_market_prediction_api.py
+++ stock_market_prediction_api.py
@@ -336,5 +336,5 @@ else:
     df_tweet = get_tweets(dict_list[selected_stock])
     tweet_text = df_tweet['Text']
     x = tweet_sentiment(tweet_text)
-    fig = plt.pie(x, values= x['sentiment'].value_counts())
-    st.write(fig)
+    fig = plt.pie(x['sentiment'].value_counts())
+    st.pyplot(fig)
